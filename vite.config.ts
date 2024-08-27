d5b9e3049a582785f0960e9be6b7ddc42cf1e2c6
stada526 <tsria526@gmail.com>
2024-08-27 11:02:33 -0400

    Update vite.config.ts to get it more robust

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
 import { defineConfig } from 'vite'
+import { resolve } from "path";
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import react from '@vitejs/plugin-react-swc'
+import { name } from "./package.json";
+
+const formattedName = name.match(/[^/]+$/)?.[0] ?? name;
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer]
+    }
+  },
   build: {
     outDir: 'dist', // default の設定と同じ
     lib: {
-      entry: 'src/index.ts',
-      name: 'myLib',
-      fileName: 'index',
+      // entry: 'src/index.ts',
+      entry: resolve(__dirname, "src/index.ts"),
+      name: formattedName,
+      // fileName: 'index',
+      fileName: "index",
       formats: ['es', 'umd'], // default の設定と同じ
     },
     rollupOptions: {
@@ -23,3 +39,4 @@ export default defineConfig({
     },
   },
 })
+
